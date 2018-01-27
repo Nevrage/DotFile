683fd8675ea17adb33c8e2ba63e97d0e64acfe2f
Nevrage <ylan.allouche@gmail.com>
2018-01-27 12:52:04 -0500

    added mactex and metropolis theme

--- distro_specific/darwin/README.md
+++ distro_specific/darwin/README.md
@@ -3,7 +3,7 @@
 ## iterm2 
 * https://github.com/fnichol/macosx-iterm2-settings
 * https://github.com/daleroberts/itermplot
-
+* brew cask install mactex
 
 ## Not sure but mac specific ... maybe ? 
 * https://github.com/PeterHajdu/iterm
--- tty/install/install.sh
+++ tty/install/install.sh
@@ -1,12 +1,23 @@
+mkdir ~/github
+
 # pipes.sh
-cd
+cd ~/github
 git clone https://github.com/pipeseroni/pipes.sh.git
 cd pipes.sh
 sudo make install
-cd
+cd ~/github
 
 #sc-im 
 sudo apt install libzip-dev libxml2-dev bison  libncurses5-dev libncursesw5-dev
 git clone https://github.com/andmarti1424/sc-im.git
 cd sc-im
 cd src && make && sudo make install 
+
+# metropolis theme for latex beamer
+cd ~/github
+git clone https://github.com/matze/mtheme.git
+cd mtheme
+make sty
+sudo make install
+cd ~/github
+

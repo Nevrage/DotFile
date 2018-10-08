d716ed7f2d12d78e33b6df39185741703c377f1b
Nevrage <ylan.allouche@gmail.com>
2018-10-08 15:26:42 +0000

    kept working on the arch install

--- platform/arch/install_arch.sh
+++ platform/arch/install_arch.sh
@@ -145,6 +145,52 @@ userdel -r admin
 # option for other de and change size of certain partition 
 # keep the scope variable
 
+sudo pacman -Sy xorg xorg-xinit firefox git python-pip vim ranger tmux neofetch R  steam gcc-fortran rofi feh htop pulseaudio alsa-utils transmission-cli mpv mupdf dunst compton  gparted docker net-tools nerd-fonts-complete pandoc texlive-most cava mutt beep scrot openssh ncmpcpp mpd mpc tk gdal zsnes gdal proj geos  lib32-gconf qutebrowser ack libreoffice mariadb
+
+# Potentially add t he following:
+# arduino blender calcurse cups dosbox dosfstools dunst fish gimp glxosd google-drive-ocamlfuse htop-vim-git nmap noto-fonts-git npm ntfs-3g oni p7zip pacutils peco python-eyed3 python-igraph quicklisp radeontop rofi-greenclip rsync samba sbcl scrot sshfs tmsu todotxt-git tremc-git uswsusp-git w3m wget wine-staging xbindkeys xclip xdotool xsel ympd-git zsnes 
+
+
+sudo pip3 install jedi rice rtv rice hangups stig  pywal wal-steam bpython ptpython jupyterlab pirate-get pandas numpy matplotlib todotxt-machine rtichoke menu4rofi buku #terminatables and jupyetr stuff 
+
+jupyter labextension install @jupyterlab/google-drive
+
+yaourt -S dropbox steam-fonts polybar-git bash-pipes ncmatrix cli-visualizer multimc5 i3-gaps zathura-pdf-poppler cool-retro-term unified-remote-server openspades jq-git udunits v8 #dofus
+
+
+# Invoke R installation here 
+
+cd /home/$user 
+git clone https://github.com/Nevrage/DotFile.git 
+# execute export.sh including xinitrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp /home/$user/DotFile/tty/vimrc ~/.vimrc
+vim +PluginInstall +qall #su here 
+
+systemctl enable docker
+systemctl start docker
+# add to docker group here 
+
+pacman -Syyu
+sudo pip install youtube_dl==2017.07.30.1
+sudo pip3 install greenlet==0.4.10
+pacman -Syyu
+
+
+#git hub stuff 
+cd
+mkdir github
+cd github
+
+git clone https://github.com/alexanderjeurissen/ranger_devicons.git
+cd ranger_devicons 
+make install
+cd ..
+
+if [ $scope = "home" ]; then
+    pacman -S steam
+    yaourt -S dropbox steam-fonts
+
 echo $host > /etc/hostname
 exit
 EOF

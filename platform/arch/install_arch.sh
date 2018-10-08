367c673d8a38df2bd89d08ea2f027d4da6d367bc
Nevrage <ylan.allouche@gmail.com>
2018-10-08 05:35:14 +0000

    Update install_arch.sh

--- platform/arch/install_arch.sh
+++ platform/arch/install_arch.sh
@@ -4,24 +4,64 @@
 # pacman -Sy curl 
     ## And source <(culr url.of/script)
 # ====================
+ 
+# change to some dialog stuff especially for drive and scope selection 
+clear
+echo "Choose a username:"
+read user 
+clear
+echo "Choose a password:"
+read pw
+clear
+echo "What is the name of this computer?"
+read host
+clear
+lsblk
+echo " 
+*********************************
+The chosen drive will be erased.
+*********************************
+"
+read drive
+clear
+echo "
+*********************************
+What is the scope of this installation?
+*********************************
 
+basic: will install barebone arch without extra user 
+server: will add more services and a non root user 
+workstation: will install all computanional tool, xorg and i3
+home: will install everything
+also available: docker and vbox
 
-## ask for usernames, passwords, drive, hostname, scope of installation: basic, server, workwstation, home
+"
+read scope
+clear
 
 echo "
 export username=user_var
-export pwd=pwd_var
-export rpw=rpw_var
+export pw=pw_var
 export drive=drive_var
 export host=host_var
-export scope=scope_var"
+export scope=scope_var" > env.sh
 
-# sed -i "s/wiki_host/${host_name}/g" /root/bin/sync
+sed -i "s/_var/${}/g" env.sh
+sed -i "s/_var/${}/g" env.sh
+sed -i "s/_var/${}/g" env.sh
+sed -i "s/_var/${}/g" env.sh
+sed -i "s/_var/${}/g" env.sh
+
 
 ## erase disk properly
+## parted rm /dev/sda 1
+## wipefs -a -n 5 or nothing 
+## dd 
+## scrub
+
 timedatectl set-ntp true
 ##### to add: ask for the name of the drive
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk $drive
   o # clear the in memory partition table
   n # new partition
   p # primary partition
@@ -46,21 +86,24 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /dev/sda
   w # write the partition table
   q # Done 
 EOF
-mkfs.ext4 /dev/sda1
-mkfs.ext4 /dev/sda4
-mkfs.ext4 /dev/sda3
-mkswap /dev/sda2
-swapon /dev/sda2
-mount /dev/sda3 /mnt
+mkfs.ext4 $drive1
+mkfs.ext4 $drive4
+mkfs.ext4 $drive3
+mkswap $drive2
+swapon $drive2
+mount $drive3 /mnt
 mkdir /mnt/boot
 mkdir /mnt/home
-mount /dev/sda1 /mnt/boot
-mount /dev/sda4 /mnt/home
+mount $drive1 /mnt/boot
+mount $drive4 /mnt/home
 pacstrap /mnt base base-devel vim ranger 
 genfstab -U /mnt >> /mnt/etc/fstab
 
-cat << EOF | arch-chroot /mnt /bin/bash
+# move the env varible now 
 
+cat << EOF | arch-chroot /mnt /bin/bash
+ 
+bash /root/env.sh
 
 pacman -S --noconfirm networkmanager git curl lm_sensors
 systemctl enable NetworkManager
@@ -71,7 +114,7 @@ echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 rm /etc/localtime
 ln /usr/share/zoneinfo/America/Montreal /etc/localtime
 pacman -S --noconfirm grub 
-grub-install --target=i386-pc /dev/sda
+grub-install --target=i386-pc $drive
 grub-mkconfig -o /boot/grub/grub.cfg
 echo -e "newuser () { \n useradd -m -g users -G audio,lp,optical,storage,video,wheel,games,power,scanner -s /bin/bash \$1 \n }" >> ~/.bashrc
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
@@ -86,6 +129,7 @@ pacman -Sy
 source ~/.bashrc
 newuser admin
 cd /root
+# embed that script 
 wget https://raw.githubusercontent.com/Nevrage/DotFile/master/platform/arch/GlobalConf.sh
 chmod755 /root/GlobalConf.sh
 su -c "/root/GlobalConf.sh" - admin
@@ -99,10 +143,17 @@ echo -e "[Service]\nExecStart=\nExecStart=-/usr/bin/agetty --autologin $user--no
 echo "root|$rpwd" | chpasswd
 pacman -Syu
 userdel -r admin
-echo $HOST > /etc/hostname
-# remove the line with the passwords
+
+# if this scope then do this 
+# how to install things with choice number 1 or all or default ?
+# option for other de and change size of certain partition 
+
+echo $host > /etc/hostname
+# remove the line with the password
 exit
 EOF
 umount -r /mnt
 reboot
+
+## run until no diff between new and curren
 

c4551e7bec81a4e7cda4603cebb41d99d4f3cd89
Nevrage <ylan.allouche@gmail.com>
2018-10-08 03:26:36 +0000

    Update GlobalConf.sh

--- platform/arch/GlobalConf.sh
+++ platform/arch/GlobalConf.sh
@@ -1,7 +1,3 @@
-source ~/.bashrc
-newuser admin
-/bin/su -c "/path/to/backup_db.sh /tmp/test" - admin
-
 cd /tmp
 git clone https://aur.archlinux.org/package-query.git
 cd package-query
@@ -11,24 +7,7 @@ git clone https://aur.archlinux.org/yaourt.git
 cd yaourt
 yes | makepkg -si
 cd ..
-
-
-
 cd /tmp
 git clone https://aur.archlinux.org/mingetty.git 
 cd mingetty 
 yes | makepkg -si
-
-
-
-
-
-
-cd
-mkdir /etc/systemd/system/getty@tty1.service.d/
-newuser $user
-echo "$user|$pwd" | chpasswd
-echo -e "[Service]\nExecStart=\nExecStart=-/usr/bin/agetty --autologin $user--noclear %I $TERM" >> /etc/systemd/system/getty@tty1.service.d/override.conf # has to become sudo again 
-echo "root|$rpwd" | chpasswd
-
-pacman -Syu

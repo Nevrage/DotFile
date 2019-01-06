9c4079c01e213aa66ad321981d97d00f21b2dce9
Nevrage <ylan.allouche@gmail.com>
2019-01-06 10:37:13 -0500

    added import script

--- import.sh
+++ import.sh
@@ -1,16 +1,41 @@
-cp ~/.bashrc ./bashrc
-cp ~/.Rprofile ./Rprofile
-cp ~/.vimrc ./vimrc 
-cp ~/.Xdefaults ./Xressources
-cp ~/.tmux.conf ./tmux.conf
-cp ~/.config/i3/config ./i3.config
-cp ~/.config/terminator/config ./terminator.config
-cp ~/.config/ranger/rc.conf ranger.rc.conf
-cp ~/.config/ranger/commands.py ./ranger_commands.py
-cp ~/.config/ranger/commands_full.py ./ranger_full_commands.py
+cp   ./tty/R/Rprofile ~/.Rprofile
+cp -r   ./tty/ranger ~/.config/ranger/*
+cp   ./tty/tmux/tmux.conf ~/.tmux.conf
+cp   ./tty/bashrc ~/.bashrc
+cp   ./tty/cava.config ~/.config/cava/config
+cp   ./tty/htoprc ~/.config/htop/htoprc 
+cp   ./tty/inputrc ~/.inputrc
+cp   ./tty/neofetch.config.conf ~/.config/neofetch/config.conf
+cp   ./tty/profile ~/.profile
+cp   ./tty/rtv.cfg ~/.config/rtv/rtv.cfg
+cp   ./tty/mailcap ~/.mailcap
+cp   ./tty/ctags ~/.ctags 
+cp   ./tty/mpd.conf ~/.mpd/mpd.conf
+cp   ./tty/transmission.settings.json ~/.config/transmission-daemon/settings.json
+cp   ./tty/vimrc ~/.vimrc
+cp   ./tty/newsboatconfig ~/.newsboat/config
+cp   ./tty/yaourtrc ~/.yaourtrc
+cp   ./tty/cava.config ~/.config/cava/config
+cp   ./tty/taskrc ~/.taskrc
+cp   ./tty/notmuch-config ~/.notmuch-config
+cp   ./tty/mutt/muttrc ~/.mutt/muttrc 
+cp   ./tty/mutt/mailcap ~/.mutt/mailcap
+cp   ./tty/davmail.properties ~/.davmail.properties 
+cp   ~/DotFile/tty/offlineimaprc ~/.offlineimaprc
+cp   ~/DotFile/tty/msmtprc ~/.msmtprc
+cp   ./tty/pirate-get ~/.config/pirate-get
+cp -r   ./tty/ncmpcpp ~/.config/ncmpcpp
 
-cp ~/.config/ranger/devicons.py ./ranger_devicons.py
-cp ~/.config/ranger/scope.sh ./ranger_scope.sh
-cp ~/.config/ranger/rifle.conf ./ranger_rifle.conf
-
-cp ~/.inputrc ./inputrc 
+cp   ./desktopSpace/gtk3/ /home/ylan/.config/gtk-3.0/*
+cp   ./desktopSpace/qutebrowser/config.py ~/.config/qutebrowser/config.py 
+cp   ./desktopSpace/qutebrowser/keys.conf ~/.config/qutebrowser/keys.conf 
+cp   ./desktopSpace/qutebrowser/style.css ~/.config/qutebrowser/style.css 
+cp   ./desktopSpace/i3/ ~/.config/i3/*
+cp    ./desktopSpace/compton.conf ~/.config/compton.conf
+cp   ./desktopSpace/polybar.config ~/.config/polybar/config
+cp   ./desktopSpace/rofi.config ~/.config/rofi/config
+cp   ./desktopSpace/xbindkeysrc ~/.xbindkeysrc
+cp   ./desktopSpace/terminator ~/.config/terminator/config
+cp   ./desktopSpace/Xdefaults ~/.Xdefaults
+cp   ./desktopSpace/xinitrc ~/.xinitrc
+cp   ~/DotFile/desktopSpace/zathurarc ~/.config/zathura/zathurarc
